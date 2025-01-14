4f9f51652d3c4ab62a650b6f868aac55ee96af3a
hafizflow <hafizur.rahman.cs@gmail.com>
2025-01-14 08:10:47 +0600

    E40

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -35,7 +35,7 @@ $user = $db->query('select * from users where email = :email', ['email' => $emai
     else {
         $db->query('INSERT INTO users (email, password) VALUES (:email, :password)',[
             'email' => $email,
-            'password' => $password
+            'password' => password_hash($password, PASSWORD_BCRYPT)
         ]);
 
         $_SESSION['user'] = [
--- views/index.view.php
+++ views/index.view.php
@@ -7,7 +7,7 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        <p>This is Home Page</p>
+        <p> Hello <?= $_SESSION['user']['email'] ?? 'Guest' ?> ,This is Home Page</p>
     </div>
 </main>
 
